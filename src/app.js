340088772784d117a1ec334ce4d26713b50e8297
Matt <63121708+themlp101@users.noreply.github.com>
2020-07-10 15:56:30 -0400

    logger.js

--- src/app.js
+++ src/app.js
@@ -3,28 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const winston = require('winston')
 
 const { NODE_ENV } = require('./config')
+const logger = require('./logger')
 
 const app = express()
-
-// set up winston
-const logger = winston.createLogger({
-	level: 'info',
-	format: winston.format.json(),
-	transports: [
-		new winston.transports.File({ filename: 'info.log' }),
-	],
-})
-
-if (NODE_ENV !== 'production') {
-	logger.add(
-		new winston.transports.Console({
-			format: winston.format.simple(),
-		})
-	)
-}
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common'
 
@@ -47,17 +30,16 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/', (req, res) => res.send('Hello, world!'))
 
-app.use(
-	(errorHandler = (error, req, res, _next) => {
-		let response
-		if (NODE_ENV === 'production') {
-			response = { error: { message: 'server error' } }
-		} else {
-			console.error(error)
-			response = { message: error.message, error }
-		}
-		res.status(500).json(response)
-	})
-)
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, _next) => {
+	let response
+	if (NODE_ENV === 'production') {
+		response = { error: { message: 'server error' } }
+	} else {
+		logger.error(error)
+		response = { message: error.message, error }
+	}
+	res.status(500).json(response)
+})
 
 module.exports = app
