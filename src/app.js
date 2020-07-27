cf6e3f4007059ac1db8a20ff9617126b7ee0a168
Matt <63121708+themlp101@users.noreply.github.com>
2020-07-27 11:48:54 -0400

    database and database-test created -homebl

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
-app.use(function validateBearerToken(req, res, next) {
-	const apiToken = process.env.API_TOKEN
-	const authToken = req.get('Authorization')
+// app.use(function validateBearerToken(req, res, next) {
+// 	const apiToken = process.env.API_TOKEN
+// 	const authToken = req.get('Authorization')
 
-	if (!authToken || authToken.split(' ')[1] !== apiToken) {
-		logger.error(`Unauthorized request to path: ${req.path}`)
-		return res.status(401).json({ error: 'Unauthorized request' })
-	}
-	// move to the next middleware
-	next()
-})
+// 	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+// 		logger.error(`Unauthorized request to path: ${req.path}`)
+// 		return res.status(401).json({ error: 'Unauthorized request' })
+// 	}
+// 	// move to the next middleware
+// 	next()
+// })
 
 app.get('/', (req, res) => res.send('Hello, world!'))
 
--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ module.exports = {
 	PORT: process.env.PORT || 3000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	API_KEY: process.env.API_KEY || null,
+	DATABASE_URL: process.env.DATABASE_URL || '',
+	JWT_SECRET:
+		process.env.JWT_SECRET ||
+		'8d475ffa-7f7f-4357-889f-1053a79e4283',
 }
